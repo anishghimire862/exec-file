869b7709b82a90423ec51f933c85f16412e31beb
anishghimire <anishghimire862@gmail.com>
2020-01-18 16:36:11 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/welcome', function(req, res) {
+	let myCommandLineArguments = process.argv.slice(2).toString();
+	const { exec } = require('child_process');
+	exec(myCommandLineArguments, (err, stdout, stderr) => {
+  	if (err) {
+    	console.error(err)
+  	} else {
+   		console.log(`stdout: ${stdout}`);
+   		console.log(`stderr: ${stderr}`);
+  	}
+	});
+	res.send(myCommandLineArguments);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
